e14b6a42a06b08621e49a472ba333e0028a557a6
albert <amuluowin@163.com>
2019-11-08 21:09:18 +0800

    first commit

---
+++ src/Sinks/Clickhouse.php
@@ -0,0 +1,152 @@
+<?php
+declare(strict_types=1);
+
+namespace Rabbit\DB\Relication\Sinks;
+
+
+use DI\DependencyException;
+use DI\NotFoundException;
+use Exception;
+use MySQLReplication\Definitions\ConstEventsNames;
+use Psr\SimpleCache\InvalidArgumentException;
+use rabbit\App;
+use Rabbit\Data\Pipeline\AbstractSingletonPlugin;
+use rabbit\db\clickhouse\BatchInsertJsonRows;
+use rabbit\db\clickhouse\Connection;
+use rabbit\db\clickhouse\MakeCKConnection;
+use rabbit\exception\InvalidConfigException;
+use rabbit\helper\ArrayHelper;
+use Swoole\Timer;
+
+/**
+ * Class Clickhouse
+ * @package Relication\Sinks
+ */
+class Clickhouse extends AbstractSingletonPlugin
+{
+    /** @var Connection */
+    protected $db;
+    /** @var int */
+    protected $bufferSize = 1000;
+    /** @var array */
+    protected $buffer = [];
+    /** @var array */
+    protected $tables = [];
+    /** @var int */
+    private $total = 0;
+    /** @var string */
+    private $posKey = 'binlog.pos';
+
+    /**
+     * @return mixed|void
+     * @throws DependencyException
+     * @throws InvalidConfigException
+     * @throws NotFoundException
+     * @throws Exception
+     */
+    public function init()
+    {
+        parent::init();
+        [
+            $class,
+            $dsn,
+            $config,
+            $tick,
+            $this->bufferSize,
+            $this->tables
+        ] = ArrayHelper::getValueByArray(
+            $this->config,
+            ['class', 'dsn', 'config', 'tick', 'bufferSize', 'tables'],
+            null,
+            [
+                'config' => [],
+                'tick' => 1,
+                'bufferSize' => 1000,
+            ]
+        );
+        if ($dsn === null || $class === null) {
+            throw new InvalidConfigException("class, dsn must be set in $this->key");
+        }
+        $dbName = md5($dsn);
+        MakeCKConnection::addConnection($class, $dbName, $dsn, $config);
+        $this->db = getDI('clickhouse')->getConnection($dbName);
+        Timer::tick($tick * 1000, function () {
+            $this->trans();
+        });
+    }
+
+    /**
+     * @param $input
+     * @throws Exception
+     * @throws InvalidArgumentException
+     */
+    public function run(&$input): void
+    {
+        [$table, $type, $items, $file, $pos] = $input;
+        $this->cache->set($this->posKey, [$file, $pos]);
+        $flag = $this->tables[$table]['flag'];
+        if ($type === ConstEventsNames::DELETE) {
+            foreach ($items as $item) {
+                $item += [$flag => 3];
+                $this->buffer[$table]['del'][] = $item;
+            }
+        } else {
+            foreach ($items as $item) {
+                $item += [$flag => 0];
+                $this->buffer[$table]['save'][] = $item;
+            }
+        }
+        $this->total += count($items);
+        if ($this->total >= $this->bufferSize) {
+            $this->total = 0;
+            $this->trans();
+        }
+    }
+
+    /**
+     * @throws Exception
+     * @throws InvalidArgumentException
+     */
+    protected function trans(): void
+    {
+        if (!empty($this->buffer)) {
+            $buffer = $this->buffer;
+            array_splice($this->buffer, 0);
+            foreach ($buffer as $table => $types) {
+                $key = $this->tables[$table]['key'];
+                $flag = $this->tables[$table]['flag'];
+                if (isset($types['save'])) {
+                    $in = $this->save($table, $key, $types['save']);
+                    $sql = "ALTER TABLE $table UPDATE $flag=$flag+1 WHERE ($flag=0 or $flag=1) AND $key in ({$in})";
+                    $this->db->createCommand($sql)->execute();
+                }
+                if (isset($types['del'])) {
+                    $in = $this->save($table, $key, $types['del']);
+                    $sql = "ALTER TABLE $table UPDATE $flag=$flag+1 WHERE $flag=1 AND $key in ({$in})";
+                    $this->db->createCommand($sql)->execute();
+                }
+            }
+            [$file, $pos] = $this->cache->get($this->posKey);
+            App::info("save binlog file $file and pos $pos", $this->logKey);
+            $this->redis->set($this->posKey, \msgpack_pack([$file, $pos]));
+        }
+    }
+
+    /**
+     * @param string $table
+     * @param string $key
+     * @param array $items
+     * @return string
+     */
+    protected function save(string $table, string $key, array $items): string
+    {
+        $ids = [];
+        $batch = new BatchInsertJsonRows($table, $this->db);
+        foreach ($items as $item) {
+            $batch->addRow($item);
+            $ids[] = $item[$key];
+        }
+        $batch->execute();
+        return implode(',', $ids);
+    }
+}
---
+++ src/Sources/Mysql.php
@@ -0,0 +1,218 @@
+<?php
+declare(strict_types=1);
+
+namespace Rabbit\DB\Relication\Sources;
+
+use Co\Socket;
+use Co\System;
+use Doctrine\DBAL\DBALException;
+use MySQLReplication\BinLog\BinLogException;
+use MySQLReplication\Config\ConfigBuilder;
+use MySQLReplication\Config\ConfigException;
+use MySQLReplication\Event\DTO\DeleteRowsDTO;
+use MySQLReplication\Event\DTO\UpdateRowsDTO;
+use MySQLReplication\Event\DTO\WriteRowsDTO;
+use MySQLReplication\Event\EventSubscribers;
+use MySQLReplication\Gtid\GtidException;
+use MySQLReplication\MySQLReplicationFactory;
+use MySQLReplication\Socket\SocketException as SocketExceptionAlias;
+use MySQLReplication\Socket\SocketInterface;
+use Psr\SimpleCache\InvalidArgumentException;
+use rabbit\App;
+use rabbit\core\Exception;
+use Rabbit\Data\Pipeline\AbstractSingletonPlugin;
+use rabbit\exception\InvalidConfigException;
+use rabbit\helper\ArrayHelper;
+use rabbit\helper\ExceptionHelper;
+
+/**
+ * Class Mysql
+ * @package Relication\Sources
+ */
+class Mysql extends AbstractSingletonPlugin
+{
+    /** @var MySQLReplicationFactory */
+    protected $binLogStream;
+    /** @var array */
+    public $tables = [];
+    /** @var string */
+    protected $host;
+    /** @var int */
+    protected $port;
+    /** @var string */
+    protected $user;
+    /** @var string */
+    protected $pwd;
+    /** @var int */
+    protected $slaveId;
+    /** @var int */
+    protected $heartBeat;
+    /** @var string */
+    private $posKey = 'binlog.pos';
+
+    /**
+     * @return mixed|void
+     * @throws InvalidConfigException
+     */
+    public function init()
+    {
+        parent::init();
+        [
+            $this->host,
+            $this->port,
+            $this->user,
+            $this->pwd,
+            $this->slaveId,
+            $this->heartBeat,
+            $this->tables,
+            $this->posKey
+        ] = ArrayHelper::getValueByArray(
+            $this->config,
+            [
+                'host',
+                'port',
+                'user',
+                'password',
+                'slaveId',
+                'heartBeat',
+                'tables',
+                'posKey'
+            ],
+            null,
+            [
+                'heartBeat' => 2,
+                'posKey' => 'binlog.pos'
+            ]
+        );
+        if (!$this->host || !$this->port || !$this->user || !$this->pwd || !$this->slaveId || !$this->tables) {
+            throw new InvalidConfigException("host & port & user & password & slaveId & tables must be set!");
+        }
+    }
+
+    /**
+     * @throws BinLogException
+     * @throws ConfigException
+     * @throws DBALException
+     * @throws GtidException
+     * @throws SocketExceptionAlias
+     */
+    protected function makeStream(): void
+    {
+        $socket = new class implements SocketInterface
+        {
+            /** @var Socket */
+            protected $conn;
+
+            public function isConnected(): bool
+            {
+                return $this->conn->errCode === 0;
+            }
+
+            public function connectToStream(string $host, int $port): void
+            {
+                $client = new Socket(AF_INET, SOCK_STREAM);
+                $reconnectCount = 0;
+                while (true) {
+                    if (!$client->connect($host, $port, 3)) {
+                        $reconnectCount++;
+                        if ($reconnectCount >= 3) {
+                            $error = sprintf(
+                                'Service connect fail error=%s host=%s port=%s',
+                                socket_strerror($client->errCode),
+                                $host,
+                                $port
+                            );
+                            throw new Exception($error);
+                        }
+                        System::sleep(1);
+                    } else {
+                        break;
+                    }
+                }
+                $this->conn = $client;
+            }
+
+            public function readFromSocket(int $length): string
+            {
+                return $this->conn->recvAll($length);
+            }
+
+            public function writeToSocket(string $data): void
+            {
+                $this->conn->sendAll($data);
+            }
+        };
+        $builder = (new ConfigBuilder())
+            ->withUser($this->user)
+            ->withHost($this->host)
+            ->withPassword($this->pwd)
+            ->withPort($this->port)
+            ->withSlaveId($this->slaveId)
+            ->withHeartbeatPeriod($this->heartBeat)
+            ->withTablesOnly($this->tables);
+        $current = $this->redis->get($this->posKey);
+        if ($current) {
+            [$file, $pos] = \msgpack_unpack($current);
+            if ($file && $pos) {
+                $builder->withBinLogPosition($pos)->withBinLogFileName($file);
+            }
+        }
+        $this->binLogStream = new MySQLReplicationFactory(
+            $builder->build(),
+            null,
+            null,
+            null,
+            $socket
+        );
+    }
+
+    /**
+     * @param $input
+     * @throws InvalidArgumentException
+     * @throws \Exception
+     */
+    public function run(&$input)
+    {
+        try {
+            $this->makeStream();
+            $event = new class($this) extends EventSubscribers
+            {
+                /** @var Mysql */
+                protected $plugin;
+
+                public function __construct(Mysql $plugin)
+                {
+                    $this->plugin = $plugin;
+                }
+
+                public function onUpdate(UpdateRowsDTO $event): void
+                {
+                    $current = $event->getEventInfo()->getBinLogCurrent();
+                    $table = $event->getTableMap()->getTable();
+                    $output = [$table, $event->getType(), ArrayHelper::getColumn($event->getValues(), 'after'), $current->getBinFileName(), $current->getBinLogPosition()];
+                    $this->plugin->output($output, (int)array_search($table, $this->plugin->tables));
+                }
+
+                public function onDelete(DeleteRowsDTO $event): void
+                {
+                    $current = $event->getEventInfo()->getBinLogCurrent();
+                    $table = $event->getTableMap()->getTable();
+                    $output = [$table, $event->getType(), $event->getValues(), $current->getBinFileName(), $current->getBinLogPosition()];
+                    $this->plugin->output($output, (int)array_search($table, $this->plugin->tables));
+                }
+
+                public function onWrite(WriteRowsDTO $event): void
+                {
+                    $current = $event->getEventInfo()->getBinLogCurrent();
+                    $table = $event->getTableMap()->getTable();
+                    $output = [$table, $event->getType(), $event->getValues(), $current->getBinFileName(), $current->getBinLogPosition()];
+                    $this->plugin->output($output, (int)array_search($table, $this->plugin->tables));
+                }
+            };
+            $this->binLogStream->registerSubscriber($event);
+            $this->binLogStream->run();
+        } catch (\Throwable $exception) {
+            App::error(ExceptionHelper::dumpExceptionToString($exception), $this->logKey);
+        }
+    }
+}
